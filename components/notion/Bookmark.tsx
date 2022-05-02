4a59576250e7960f8ceece0435e87a2313e9aef7
Borja Leiva <borja.leiva@alpima.com>
2022-05-02 17:14:41 +0100

    :sparkles: Improve bullet styles and bookmark component

--- components/notion/Bookmark.tsx
+++ components/notion/Bookmark.tsx
@@ -7,17 +7,15 @@ export interface BookmarkProps {
 }
 
 export default function Bookmark({ url = '', title = '', description = '', image = '' }: BookmarkProps) {
-  const truncatedTitle = description.length > 70 ? `${description.substring(0, 70)}...` : description;
-  const maxDescLength = description.length > 70 ? 70 : 120;
-  const truncatedDesc =
-    description.length > maxDescLength ? `${description.substring(0, maxDescLength)}...` : description;
+  const truncatedTitle = title.length > 70 ? `${title.substring(0, 70)}...` : title;
+  const truncatedDesc = description.length > 200 ? `${description.substring(0, 200)}...` : description;
 
   return (
-    <a href={url} className="my-4 max-w-xl xs:h-32">
+    <a href={url} className="my-4 max-w-xl xs:h-32" target="_blank" rel="noopener noreferrer">
       <div className="border border-slate-300 dark:border-neutral-800 rounded-md flex flex-wrap flex-col-reverse xs:flex-row hover:bg-slate-50 dark:hover:bg-neutral-900">
         <div className="flex flex-col p-3 w-full xs:w-3/5">
           <h4 className="font-bold">{truncatedTitle}</h4>
-          <p className="hidden xs:flex">{truncatedDesc}</p>
+          <p className="hidden text-base xs:flex">{truncatedDesc}</p>
         </div>
         <div className="w-full xs:w-2/5 h-full bg-transparent rounded-tl-md rounded-tr-md xs:rounded-tl-none xs:rounded-tr-md xs:rounded-br-md">
           <img
